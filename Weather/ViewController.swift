608061642d7c5540c3ddac13ce2858744b1464a2
Su Yan <suyan@sy-rmbp.local>
2015-12-07 23:01:19 -0800

    init project

---
+++ Weather/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Su Yan on 12/7/15.
+//  Copyright © 2015 suyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var statePicker: UIPickerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
