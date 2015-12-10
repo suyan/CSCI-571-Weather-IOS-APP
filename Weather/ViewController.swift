2a0998341967ece31a112dd13d3c219864d0631f
Su Yan <suyan0830@gmail.com>
2015-12-10 14:49:40 -0800

    change radio buttons

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -14,15 +14,14 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var stateTextField: UITextField!
     @IBOutlet weak var statePicker: UIPickerView!
-    @IBOutlet weak var degreeTextField: UITextField!
-    @IBOutlet weak var degreePicker: UIPickerView!
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var forecastButton: UIButton!
+    @IBOutlet weak var fButton: UIButton!
+    @IBOutlet weak var cButton: UIButton!
+    
 
     let stateValues = ["Select","Alabama","Alaska","American Samoa","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District Of Columbia","Federated States Of Micronesia","Florida","Georgia","Guam","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Marshall Islands","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Northern Mariana Islands","Ohio","Oklahoma","Oregon","Palau","Pennsylvania","Puerto Rico","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virgin Islands","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
     let stateKeys = ["NULL","AL","AK","AS","AZ","AR","CA","CO","CT","DE","DC","FM","FL","GA","GU","HI","ID","IL","IN","IA","KS","KY","LA","ME","MH","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","MP","OH","OK","OR","PW","PA","PR","RI","SC","SD","TN","TX","UT","VT","VI","VA","WA","WV","WI","WY"]
-    let degreeValues = ["Fahrenheit", "Celsius"]
-    let degreeKeys = ["f", "c"]
     
     var stateKey = "NULL"
     var degreeKey = "f"
@@ -32,17 +31,13 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
 
         // connect data
         self.statePicker.delegate = self
-        self.degreePicker.delegate = self
         self.streetTextField.delegate = self
         self.cityTextField.delegate = self
         self.stateTextField.delegate = self
-        self.degreeTextField.delegate = self
 
         statePicker.hidden = true
-        degreePicker.hidden = true
         errorLabel.text = ""
         stateTextField.text = stateValues[0]
-        degreeTextField.text = degreeValues[0]
         
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg")!)
     }
@@ -53,11 +48,26 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         stateTextField.text = stateValues[0]
         stateKey = stateKeys[0]
         statePicker.selectRow(0, inComponent: 0, animated: true)
-        degreeTextField.text = degreeValues[0]
-        degreeKey = degreeKeys[0]
-        degreePicker.selectRow(0, inComponent: 0, animated: true)
         errorLabel.text = ""
     }
+    
+    @IBAction func chooseF() {
+        degreeKey = "f"
+        fButton.backgroundColor = UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+        cButton.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        fButton.setTitleColor(UIColor(red: 1, green: 1, blue: 1, alpha: 1), forState: UIControlState.Normal)
+        cButton.setTitleColor(UIColor(red: 0, green: 0, blue: 0, alpha: 1), forState: UIControlState.Normal)
+
+    }
+    
+    @IBAction func chooseC() {
+        degreeKey = "c"
+        cButton.backgroundColor = UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+        fButton.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        cButton.setTitleColor(UIColor(red: 1, green: 1, blue: 1, alpha: 1), forState: UIControlState.Normal)
+        fButton.setTitleColor(UIColor(red: 0, green: 0, blue: 0, alpha: 1), forState: UIControlState.Normal)
+    }
+    
     
     // MARK: picker delgate
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
@@ -65,44 +75,25 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView == statePicker {
-            return stateValues.count
-        } else {
-            return degreeValues.count
-        }
+        return stateValues.count
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView == statePicker {
-            return stateValues[row]
-        } else {
-            return degreeValues[row]
-        }
+        return stateValues[row]
     }
 
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if pickerView == statePicker {
-            stateTextField.text = stateValues[row]
-            stateKey = stateKeys[row]
-            statePicker.hidden = true
-        } else {
-            degreeTextField.text = degreeValues[row]
-            degreeKey = degreeKeys[row]
-            degreePicker.hidden = true
-        }
+        stateTextField.text = stateValues[row]
+        stateKey = stateKeys[row]
+        statePicker.hidden = true
+
     }
     
     // MARK: textfield delgate
     // when click textfield
     func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
-        if textField == degreeTextField {
+        if textField == stateTextField {
             self.view.endEditing(true)
-            statePicker.hidden = true
-            degreePicker.hidden = false
-            return false
-        } else if textField == stateTextField {
-            self.view.endEditing(true)
-            degreePicker.hidden = true
             statePicker.hidden = false
             return false
         } else {
