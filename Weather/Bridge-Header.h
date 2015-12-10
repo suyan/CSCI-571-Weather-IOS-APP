be9e6580ea59da8c31f9a1c7bb699b63a0a9e4ed
Su Yan <suyan0830@gmail.com>
2015-12-09 16:55:03 -0800

    finish Facebook sharing

---
+++ Weather/Bridge-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridge-Header.h
+//  Weather
+//
+//  Created by Su Yan on 12/9/15.
+//  Copyright © 2015 suyan. All rights reserved.
+//
+
+#ifndef Bridge_Header_h
+#define Bridge_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
+
+#endif /* Bridge_Header_h */
--- Weather/ResultViewController.swift
+++ Weather/ResultViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ResultViewController: UIViewController {
+class ResultViewController: UIViewController, FBSDKSharingDelegate {
 
     @IBOutlet weak var summaryImage: UIImageView!
     @IBOutlet weak var summaryLabel: UILabel!
@@ -23,9 +23,14 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var sunrise: UILabel!
     @IBOutlet weak var sunset: UILabel!
     
+    var imageName = ""
+    var baseSummary = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         var data = WeatherData.sharedInstance.data
+        self.imageName = data["image_name"]! as! String
+        self.baseSummary = data["base_summary"]! as! String
         summaryImage.image = UIImage(named: data["image_name"]! as! String)
         summaryLabel.text = data["summary"] as? String
         tempLabel.text = data["temp_label"] as? String
@@ -38,5 +43,39 @@ class ResultViewController: UIViewController {
         visibility.text = data["visibility"] as? String
         sunrise.text = data["sunrise"] as? String
         sunset.text = data["sunset"] as? String
+
+        // fb delgate
+        FBSDKApplicationDelegate.self;
     }
+    
+    @IBAction func facebookShare() {
+        let content = FBSDKShareLinkContent()
+        content.contentURL = NSURL(string: "http://forecast.io")
+        content.contentTitle = summaryLabel.text
+        content.contentDescription = self.baseSummary + ", " + tempLabel.text!
+        content.imageURL = NSURL(string: "http://cs-server.usc.edu:45678/hw/hw8/images/\(self.imageName).png");
+        
+        FBSDKShareDialog.showFromViewController(self, withContent: content, delegate: self);
+
+        //FBSDKShareDialog.showFromViewController(self, withContent: content, delegate: nil)
+        let button:FBSDKShareButton = FBSDKShareButton()
+        button.frame = CGRectMake(0, 0, 0, 0)
+        button.shareContent = content
+        button.sendActionsForControlEvents(UIControlEvents.TouchUpInside)
+    }
+    
+    // Facebook Delegate Methods
+    func sharer(sharer: FBSDKSharing!, didCompleteWithResults results: [NSObject: AnyObject]) {
+        print("share success")
+    }
+    
+    func sharer(sharer: FBSDKSharing!, didFailWithError error: NSError!) {
+        print("sharer NSError")
+        print(error.description)
+    }
+    
+    func sharerDidCancel(sharer: FBSDKSharing!) {
+        print("sharerDidCancel")
+    }
+    
 }
